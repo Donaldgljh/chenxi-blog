6e8ef8a90fe1ecdfd7decfa8e55231209112f137
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-07-03 07:14:33 +0000

    Deploy Donaldgljh/chenxi-blog to github.com/Donaldgljh/chenxi-blog.git:gh-pages

---
+++ assets/index.7b7f95a3.js
@@ -0,0 +1,1 @@
+import{R as e}from"./vendor.79bc104b.js";import{C as r}from"./index.b60e8fff.js";export default()=>e.createElement(r,null);
--- assets/index.8dfa070b.js
+++
@@ -1,1 +0,0 @@
-import{R as e}from"./vendor.79bc104b.js";import{C as r}from"./index.ac57ba89.js";export default()=>e.createElement(r,null);
